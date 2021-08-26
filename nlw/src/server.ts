d90c55fe2a7bddd4729543cca73451dd86c687fe
italo lima <italo.itlima@gmail.com>
2021-08-26 20:45:06 -0300

    NLW Day Two

--- nlw/src/server.ts
+++ nlw/src/server.ts
@@ -1,14 +1,12 @@
+import "reflect-metadata";
 import express from "express";
+import { router } from "./routes";
+import "./database";
 
 const app = express();
 
-app.get("/test", (request, response) => {
-  return response.send("Olá Ítalo");
-});
+app.use(express.json());
 
-app.post("/test-post", (request, response) =>{
-  return response.send("Olá Ítalo método post");
-});
+app.use(router);
 
-//http://localhost:3000
 app.listen(3000, () => console.log("server is running"));
